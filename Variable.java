6b49ee21563e4f344a6a90412bc470ee13239982
SheetalMulmi <131242077+SheetalMulmi@users.noreply.github.com>
2023-11-21 08:06:38 +0545

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,20 @@
+package homeworks;
+
+public class Variable {
+    public static void main(String[] args) {
+        String a="Sheetal Mulmi";
+        String b="Hello World";
+
+        System.out.println(a.equals(b));
+        System.out.println(a.equalsIgnoreCase(b));
+        System.out.println(!a.equals(b));
+        System.out.println(a.toLowerCase());
+        System.out.println(a.toUpperCase());
+        System.out.println(a.length());
+        System.out.println(a.indexOf("e"));
+        System.out.println(a.charAt(2));
+        System.out.println(a.contains("e"));
+
+    }
+}
+
